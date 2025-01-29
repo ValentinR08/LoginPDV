f52d873271e206c5ffbb77cedcd7c19d3e7eaf71
valentinR08 <valentonriversan@gmail.com>
2025-01-29 10:08:15 -0600

    tercera version

--- script.js
+++ script.js
@@ -1,6 +1,34 @@
-const cashRegister = document.querySelector('.cash-register');
-const drawer = document.querySelector('.cash-register .drawer');
+const table = document.querySelector('.table');
+const belt = document.querySelector('.conveyor-belt');
 
-cashRegister.addEventListener('click', () => {
-    drawer.classList.toggle('open');
-});
+
+function addProduct(){
+    let newProduct = document.createElement('div');
+    newProduct.classList.add('product');
+    table.appendChild(newProduct)
+}
+
+function removeProduct(){
+    let product = belt.querySelector('.product')
+    belt.removeChild(product)
+}
+function addProductBelt(){
+    let newProduct = document.createElement('div');
+    newProduct.classList.add('product');
+    belt.appendChild(newProduct)
+}
+function engineProduct() {
+    for (let i = 0; i < 4; i++) {
+        setTimeout(() => {
+             addProduct()
+             removeProduct()
+        },i*10000);   
+    }
+
+    setTimeout(() => {
+        engineProduct();
+    }, 4 * 20000);
+}
+
+engineProduct();
+
